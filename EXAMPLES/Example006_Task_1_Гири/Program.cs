078f4e5e99c5ac70ea84aa0fc3fc0af77034fe99
e.tairov <ev.tairov@yandex.ru>
2023-01-15 19:29:38 +0700

    Example006_Гири

---
+++ EXAMPLES/Example006_Task_1_Гири/Program.cs
@@ -0,0 +1,13 @@
+﻿int box1 = 3;
+int box2 = 5;
+int box3 = 13;
+int box4 = 8;
+int box5 = 4;
+int Max = box1;
+if(box2 > Max) Max = box2;
+if(box3 > Max) Max = box3;
+if(box4 > Max) Max = box4;
+if(box5 > Max) Max = box5;
+
+Console.Write("Max = ");
+Console.WriteLine(Max);
