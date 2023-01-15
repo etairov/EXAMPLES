26d4d09e1aa25f1a6558c2918ca1a5f3c7a710d8
e.tairov <ev.tairov@yandex.ru>
2023-01-15 17:52:53 +0700

    Example004_If_Else

---
+++ EXAMPLES/Example005_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите ваше имя!");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "женя")
+{
+Console.WriteLine("УРА!!! Это же Женя!!!");
+}
+else
+{
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
+}
