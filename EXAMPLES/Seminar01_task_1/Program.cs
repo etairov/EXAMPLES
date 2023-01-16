75132b27fca80cb4f7cf6294560565bb4e15647b
e.tairov <ev.tairov@yandex.ru>
2023-01-16 09:19:28 +0700

    Seminar001_task_1

--- EXAMPLES/Seminar01_task_1/Program.cs
+++ EXAMPLES/Seminar01_task_1/Program.cs
@@ -12,3 +12,14 @@ Console.Write("Число = ");
 Console.WriteLine(number);
 Console.Write("Квадрат числа = ");
 Console.WriteLine(sqrt);
+
+Вариант со вводом числа с консоли.
+//Console.Write("Введите число");
+//string numberStr = Console.ReadLine(); //Консоль всегда воспринимает только строчные данные.
+//int number = Convert.ToInt32(numberStr); //Конвертация в тип данных int.
+
+//int result = number * number;
+
+//Console.WriteLine($"Квадрат числа {number} = {result}"); //Так сейчас выводят рез-т в продакшн(через интерполяцию строк $)
+//Console.WriteLine("Квадрат числа" + number + " = " + result); //Так делали до появления интерполяции
+//Console.WriteLine("Квадрат числа {0} = {1}", number, result); //Один из вариантов
