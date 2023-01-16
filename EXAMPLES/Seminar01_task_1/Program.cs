d0408e2efe1171e75b695f06b95987e8251585b0
e.tairov <ev.tairov@yandex.ru>
2023-01-16 08:48:07 +0700

    Seminar001_task_1

---
+++ EXAMPLES/Seminar01_task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное
+//на само себя).
+
+//Например:
+//4 -> 16
+//-3 -> 9
+//-7 -> 49
+
+int number = new Random().Next(0, 9);
+int sqrt = number * number;
+Console.Write("Число = ");
+Console.WriteLine(number);
+Console.Write("Квадрат числа = ");
+Console.WriteLine(sqrt);
