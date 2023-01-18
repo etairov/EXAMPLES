5e945fc1bc00e521d1254eca444b28a6feb9a0f1
e.tairov <ev.tairov@yandex.ru>
2023-01-18 16:41:21 +0700

    Example011_FinTask

---
+++ Example011_FinTask/Program.cs
@@ -0,0 +1,51 @@
+﻿//Финальное упражнение Лекции №2
+//--------------------------------
+
+void FillArray(int[] collection)//Метод, заполняющий массив
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(0, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)//Метод, печатающий массив
+{
+    int count = col.Length;
+    int position = 0;//Аналог переменной index
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;//При int position = -1, при вводе числа, превышающего границы массива, программа выводит -1
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+int[] array = new int[10];//Создать новый массив с кол-вом эл-тов- 10
+//Массив по умолчанию заполнен нулями, чтобы его заполнить числами, необходим метод выше.
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
