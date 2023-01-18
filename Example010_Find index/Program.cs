71e2f59c1d7777ab7a28aa2d23cc5c626ae269d0
e.tairov <ev.tairov@yandex.ru>
2023-01-18 14:20:48 +0700

    Example010_Array

---
+++ Example010_Find index/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача на нахождение индекса элемента массива
+
+int[] array = {1, 3, 15, 6, 8, 3, 4};
+int n = array.Length;
+int find = 3;
+int index = 0; 
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+    Console.WriteLine(index);
+    break;       
+    }    
+    index++;
+}
