2f91b83bf9f5f8cb5c8c9e90fbd3bcea45f2bb8f
e.tairov <ev.tairov@yandex.ru>
2023-01-24 02:31:49 +0700

    Seminar03_task_4

--- Seminar03_task_4/Program.cs
+++ Seminar03_task_4/Program.cs
@@ -8,11 +8,32 @@
 Console.WriteLine("Введите число n");
 string nStr = Console.ReadLine();
 int n = Convert.ToInt32(nStr);
+double quadra = n;
+int count = 0;
 
-while (int count <= n)
+Console.Write($"Квадраты чисел от 1 до {n} ---> ");
+
+while (count <= n)
 {
-    double quadra = Math.Pow(1, n);
+    quadra = Math.Pow(count, 2);
+    if (count < n)
+    {
+        Console.Write(quadra + ", ");    
+    }
+    else
+    {
+        Console.Write(quadra + "  ");
+    }
+    count++;
 }
-count++;
+//-----------------------------------------
+//Решение группы
 
-Console.WriteLine($"Квадраты чисел от 1 до {n} -> {quadra}")
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+
+for (int i = 1; i <= number; i++)
+{
+Console.Write($"{i*i},");
+}
