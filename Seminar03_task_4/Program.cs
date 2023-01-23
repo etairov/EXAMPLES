ca4063397fe4f691b706b4a1f304a9b3c5c71835
e.tairov <ev.tairov@yandex.ru>
2023-01-24 00:32:14 +0700

    Begin

--- Seminar03_task_4/Program.cs
+++ Seminar03_task_4/Program.cs
@@ -3,3 +3,16 @@
 
 //5 -> 1, 4, 9, 16, 25.
 //2 -> 1,4
+//------------------------------
+
+Console.WriteLine("Введите число n");
+string nStr = Console.ReadLine();
+int n = Convert.ToInt32(nStr);
+
+while (int count <= n)
+{
+    double quadra = Math.Pow(1, n);
+}
+count++;
+
+Console.WriteLine($"Квадраты чисел от 1 до {n} -> {quadra}")
