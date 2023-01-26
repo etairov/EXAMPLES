ed7d8b9258a6dd5a5f709c113a7cfc2f781056ae
e.tairov <ev.tairov@yandex.ru>
2023-01-26 17:29:44 +0700

    04_1

---
+++ Seminar04_task_1/Program.cs
@@ -0,0 +1,59 @@
+﻿//Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+//7 -> 28
+//4 -> 10
+//8 -> 36
+//---------------------
+
+int GetNumber()
+{
+    Console.WriteLine("Введите любое число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int resultGetNumber = GetNumber();
+
+int A = resultGetNumber;
+
+    Console.Write($"{resultGetNumber} -> ");
+    Console.Write($"{A * (A + 1) / 2} ");
+
+//---------------------------------------------
+// Решение преподавателя
+
+int GetNumber(string message)
+{
+int result = 0;
+
+while(true)
+{
+Console.WriteLine(message);
+
+if(int.TryParse(Console.ReadLine(), out result) && result > 1)
+{
+break;
+}
+else
+{
+Console.WriteLine("Ввели не число. Повторите ввод");
+}
+}
+
+return result;
+}
+
+int GetSumm(int number)
+{
+int sum = 0;
+
+for (int i = 1; i <= number; i++)
+{
+sum += i;
+}
+
+return sum;
+}
+
+int number = GetNumber("Введите число больше 1");
+int sum = GetSumm(number);
+Console.WriteLine($"сумма чисел от 1 до {number} = {sum}");
+
