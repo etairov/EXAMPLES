2a37e761f8bb09a821f2fc900708adade1cc3244
e.tairov <ev.tairov@yandex.ru>
2023-02-07 19:37:50 +0700

    Sem07_4

--- Seminar07_task_4/Program.cs
+++ Seminar07_task_4/Program.cs
@@ -6,7 +6,6 @@
 //Сумма элементов главной диагонали: 1+9+2 = 12
 //---------------------------------------------
 
-
 int GetNumber(string message)
 {
     int result = 0;
@@ -83,3 +82,76 @@ Console.WriteLine();
 
 MainDiagonal(matrix);
 
+//--------------------------------
+//Решение группы
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не корректное число. Повторите ввод.");
+        }
+    }
+
+    return result;
+}
+
+int[,] InitMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = (i + j);
+        }
+    }
+
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int SumElementsDiagonal (int [,] matrix)
+{
+int sum = 0;
+int count = matrix.GetLength(0);
+if(count > matrix.GetLength(1))
+count = matrix.GetLength(1);
+
+for (int i = 0; i < count; i++)
+{
+sum = sum + matrix [i,i];
+}
+
+return sum;
+}
+
+int rows = GetNumber("Введите количество строк: ");
+int columns = GetNumber("Введите количество столбцов: ");
+int [,] matrix = InitMatrix(rows, columns);
+PrintMatrix(matrix);
+int sum = SumElementsDiagonal(matrix);
+Console.WriteLine($"Сумма элементов, находящихся на главной диагонали = {sum}");
