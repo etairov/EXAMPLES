853222738b2a7166e2150cb18095db240bf3078c
e.tairov <ev.tairov@yandex.ru>
2023-02-07 16:42:29 +0700

    Sem07_3

---
+++ Seminar07_task_3/Program.cs
@@ -0,0 +1,67 @@
+﻿//Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+//Например, изначально массив
+//выглядел вот так:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+
+
+//-----------------------------
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не корректное число. Повторите ввод.");
+        }
+    }
+
+    return result;
+}
+double[,] InitMatrix(int rows, int columns)
+{
+    double[,] matrix = new double[rows, columns];
+    //Random rnd = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = 10;//rnd.Next(1, 10);
+
+            if (i % 2 == 0 && j % 2 == 0)
+            matrix[i, j] = Math.Pow(matrix[i, j], 2);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+int countOfRows = GetNumber("Введите кол-во строк:");
+int countOfColumns = GetNumber("Введите кол-во столбцов:");
+double[,] matrix = InitMatrix(countOfRows, countOfColumns);
+
+PrintMatrix(matrix);
