87beb5bd953c6712c92f6c8b94df8e9b1c3f8020
e.tairov <ev.tairov@yandex.ru>
2023-02-07 17:29:20 +0700

    Sem07_3

--- Seminar07_task_3/Program.cs
+++ Seminar07_task_3/Program.cs
@@ -40,7 +40,7 @@ double[,] InitMatrix(int rows, int columns)
             matrix[i, j] = 10;//rnd.Next(1, 10);
 
             if (i % 2 == 0 && j % 2 == 0)
-            matrix[i, j] = Math.Pow(matrix[i, j], 2);
+                matrix[i, j] = Math.Pow(matrix[i, j], 2);
         }
     }
 
@@ -64,4 +64,75 @@ int countOfRows = GetNumber("Введите кол-во строк:");
 int countOfColumns = GetNumber("Введите кол-во столбцов:");
 double[,] matrix = InitMatrix(countOfRows, countOfColumns);
 
+PrintMatrix(matrix);
+
+//-------------------------------
+//Решение группы через отдельную функцию и со смещением значения четности
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не корректное число. Повторите ввод.");
+        }
+    }
+
+    return result;
+}
+
+int[,] InitMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = (i + j);
+        }
+    }
+
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void FindEvenIndex(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 1 && j % 2 == 1)
+            matrix[i, j] = matrix[i, j] * matrix[i, j];
+        }
+    }
+}
+int countOfRows = GetNumber("Введите кол-во строк:");
+int countOfColumns = GetNumber("Введите кол-во столбцов:");
+int[,] matrix = InitMatrix(countOfRows, countOfColumns);
+
+PrintMatrix(matrix);
+FindEvenIndex(matrix);
 PrintMatrix(matrix);
