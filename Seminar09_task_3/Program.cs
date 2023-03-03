ac80b6c17071629a0649213ad649971d4a8c9d37
e.tairov <ev.tairov@yandex.ru>
2023-03-03 14:07:53 +0700

    Sem09_3

---
+++ Seminar09_task_3/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9
+*/
+//------------------------
+
+int SumOfDigits(int number, int sum)
+{
+    if (number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+        return SumOfDigits(number, sum);
+    }
+    else
+        return sum;
+}
+int sum = SumOfDigits(5, 9);
+Console.WriteLine(sum);
+
+//-----------------------------
+
+int SumOfDigits(int number, int sum)
+{
+if (number > 0)
+{
+sum += number % 10;
+number /= 10;
+return SumOfDigits(number, sum);
+}
+else
+return sum;
+}
+Console.WriteLine (SumOfDigits(5, 8));
